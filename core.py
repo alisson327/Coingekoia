ecd4ab38e5281c3ba1e4b31b2ebb92666ec75876
alisson327 <152733695+alisson327@users.noreply.github.com>
2025-06-12 20:38:02 -0300

    Update core.py

--- core.py
+++ core.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 def get_data_coingecko():
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
-    params = {
-        "vs_currency": "usd",
-        "days": "1",
-        "interval": "minutely"
-    }
+params = {
+    "vs_currency": "usd",
+    "days": "1",
+    "interval": "minutely"
+}
+    
     try:
         response = requests.get(url, params=params)
         data = response.json()
