096d0aba87c6d67ef9be2b2354a17a2b56112f54
alisson327 <152733695+alisson327@users.noreply.github.com>
2025-06-12 20:29:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from core import get_data_coingecko, gerar_sinal
 from telegram_bot import enviar_sinal
 
 if __name__ == "__main__":
-    enviar_sinal("✅ Teste de mensagem enviada com sucesso!")
+    df = get_data_coingecko()
+    if df.empty:
+        print("⚠️ Nenhum dado retornado da CoinGecko")
+    else:
+        sinal = gerar_sinal(df)
+        if sinal:
+            enviar_sinal(sinal)
