3a7fb5dcfa623f32616a33e2150fa6919ade8ff9
alisson327 <152733695+alisson327@users.noreply.github.com>
2025-06-13 19:32:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from nucleo import get_data_coingecko, gerar_sinal
+from get_data_fake import get_data_coingecko
+from nucleo import gerar_sinal
 from telegram_bot import enviar_sinal
 
 if __name__ == "__main__":
     df = get_data_coingecko()
     if df.empty:
-        print("⚠️ Nenhum dado retornado da CoinGecko")
+        print("⚠️ Nenhum dado retornado da fonte")
     else:
         sinal = gerar_sinal(df)
         if sinal:
