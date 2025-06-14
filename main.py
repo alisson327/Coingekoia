227b278c3ff9ce9172fe2ff892ba31df79a05281
alisson327 <152733695+alisson327@users.noreply.github.com>
2025-06-14 14:18:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from nucleo import get_data_coingecko, gerar_sinal
+from nucleo import get_data_yahoo, gerar_sinal
 from telegram_bot import enviar_sinal
 
 if __name__ == "__main__":
-    df = get_data_coingecko()
+    df = get_data_yahoo()
     if df.empty:
         print("⚠️ Nenhum dado retornado")
     else:
